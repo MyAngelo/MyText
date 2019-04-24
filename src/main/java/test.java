351df6155a9d50e7906ff6599ee5de15e38c65ea
MyAngelo <1351750668@qq.com>
2019-04-24 13:12:42 +0800

    first commit

---
+++ src/main/java/test.java
@@ -0,0 +1,11 @@
+/**
+ * 描述:
+ *
+ * @author cheng
+ * @create 2019-04-24 13:11
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
